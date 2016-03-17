770524490d7278792e0fc751f05f58bebaebbb88
diek <diek@bellaliant.net>
2016-03-17 17:42:37 -0300

    added CBVs

--- books/views.py
+++ books/views.py
@@ -1,5 +1,7 @@
+from django.db.models import Count
 from django.shortcuts import render
-from .models import Book
+from django.views.generic import DetailView, View
+from .models import Author, Book
 
 
 def list_books(request):
@@ -7,6 +9,7 @@ def list_books(request):
     List all the books that have reviews
     """
 
+    # use prefetch_related, the authors, and just reference it, vice query the db very time.
     books = Book.objects.exclude(date_reviewed__isnull=True).prefetch_related('authors')
 
     context = {
@@ -14,4 +17,32 @@ def list_books(request):
     }
 
     return render(request, 'books/list.html', context)
+
+
+# Class Based View
+class AuthorList(View):
+    def get(self, request):
+
+        # authors = Author.objects.all()
+        authors = Author.objects.annotate(
+            published_books=Count('books')).filter(
+                # count > 0, note the double underscore
+                published_books__gt=0)
+
+        context = {
+            'authors': authors
+        }
+
+        return render(request, 'books/authors.html', context)
+
+
+# Class Based View - Generic
+class BookDetail(DetailView):
+    model = Book
+    template_name = 'books/book.html'
+
+
+class AuthorDetail(DetailView):
+    model = Author
+    template_name = 'books/author.html'
 
--- read_it/urls.py
+++ read_it/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
-from books.views import list_books
+from books.views import AuthorDetail, AuthorList, BookDetail, list_books
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', list_books, name='books'),
+    # CBVs need as_view()
+    url(r'^authors/$', AuthorList.as_view(), name='authors'),
+    url(r'^authors/(?P<pk>[-\w]+)/$', AuthorDetail.as_view(), name='author-detail'),
+    url(r'^books/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name='book-detail'),
 ]
